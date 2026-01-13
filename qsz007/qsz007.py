59bd03367df2b73d1d22e8b35556d788aeada265
JesseChu1018 <jessechu1018@quantaser.com>
2026-01-13 15:03:43 +0800

    Update qsz007.py

--- qsz007/qsz007.py
+++ qsz007/qsz007.py
@@ -35,10 +35,10 @@ class SOC(Overlay):
 
         self.__init_socip()
 
-        # if download:
-        #     self.__avtt_set(voltage="2.5V")
-        #     time.sleep(0.5)
-        #     self.__balun_init()
+        if download:
+            self.__avtt_set(voltage="2.5V")
+            time.sleep(0.5)
+            self.__balun_init()
 
     def __getitem__(self, key):
         return self._cfg[key]
